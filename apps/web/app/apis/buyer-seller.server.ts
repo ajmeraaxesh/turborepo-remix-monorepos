7410b0c276465b7f021dba398999af5da9f5663a
Axesh R. Ajmera <ajmeraaxesh@gmail.com>
2022-11-07 14:01:20 +0530

    Added prettier and formatting file using prettier

--- apps/web/app/apis/buyer-seller.server.ts
+++ apps/web/app/apis/buyer-seller.server.ts
@@ -6,20 +6,20 @@ const BUYER_SELLER_URL = process.env.BUYER_SELLER_URL
 
 
 // export const getUserList = async (request) => {
-    //     const response = await fetch(`${BUYER_SELLER_URL}/api/settings/user/list`, {
-        //         method: 'POST',
-        //         headers: {
-            //             ...commonHeaders
-            //         },
-            //         body: JSON.stringify(apiBody),
-            //     })
+//     const response = await fetch(`${BUYER_SELLER_URL}/api/settings/user/list`, {
+//         method: 'POST',
+//         headers: {
+//             ...commonHeaders
+//         },
+//         body: JSON.stringify(apiBody),
+//     })
 
-            //     const data = await response.json()
-            //     //console.log("JSON data ",   data)
-            //     return data
-            // }
+//     const data = await response.json()
+//     //console.log("JSON data ",   data)
+//     return data
+// }
 //TODO: Get types from buyer-seller entity and map it out
-export const getApprovedBuyersAndSellers = async (request: Request ,filters = {}, pageSize = 100) => {
+export const getApprovedBuyersAndSellers = async (request: Request, filters = {}, pageSize = 100) => {
     const commonHeaders = await getApiCommonHeaders(request)
     const response = await fetch(`${BUYER_SELLER_URL}/entity?filters={"adminApproval":"1"}&limit=${pageSize}`, {
         method: 'GET',
